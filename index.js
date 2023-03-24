0b003cd7d8453ab63b655f08adffb1becc9de0fe
VAK <vak@pina.su>
2023-03-24 20:34:30 +0500

    new file:   index.html new file:   index.js

---
+++ index.js
@@ -0,0 +1,15 @@
+const PvC = document.getElementById('PvC');
+const PvP = document.getElementById('PvP');
+
+function main(){
+
+    PvC.addEventListener('click', function(){
+        window.open("/1. RSP_PvC/pvc_game.html");
+    })
+
+    PvP.addEventListener('click', function(){
+        window.open("/1. RSP_PvC/pvc_game.html");
+    })
+    
+}
+main();
